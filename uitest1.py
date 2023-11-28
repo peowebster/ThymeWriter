20619c84b62936ad6d906f27ddc83258b752304f
peowebster <entropicthymes@outlook.com>
2023-11-28 09:06:08 +0000

    Start Window closes when Words Window opened

--- uitest1.py
+++ uitest1.py
@@ -29,6 +29,7 @@ class StartWindow(Tk):
 
     def handle_words_press(self):
         wordswindow = WordsWindow()
+        self.destroy()
         wordswindow.mainloop()
 
 class WordsWindow(Tk):
@@ -37,6 +38,30 @@ class WordsWindow(Tk):
 
         self.title("ThymeWriter")
         self.geometry("1200x700")
+
+        # search bar
+        self.searchbar = Entry()
+        self.searchbar.pack(side="right", anchor="ne")
+
+        # tab buttons
+        self.wordstab = Button(text="Home", command=self.handle_home_press)
+        self.wordstab.pack(side="left", anchor="nw")
+        self.achtab = Button(text="Achievements")
+        self.achtab.pack(side="left", anchor="nw")
+        self.writetab = Button(text="Writer")
+        self.writetab.pack(side="left", anchor="nw")
+        self.notetab = Button(text="Notes")
+        self.notetab.pack(side="left", anchor="nw")
+        
+        #logo
+        self.logoimage = PhotoImage(file='./assets/thymewriterlogo3.png')
+        self.logolabel = Label(image=self.logoimage)
+        self.logolabel.pack()
+
+    def handle_home_press(self):
+        window = StartWindow()
+        self.destroy()
+        window.mainloop()
 
 # Start the event loop.
 window = StartWindow()
