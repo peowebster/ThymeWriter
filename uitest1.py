bcf956ad1afca045ecc6a0a7241a6bf40ffbbf09
peowebster <entropicthymes@outlook.com>
2023-12-11 12:19:31 +0000

    Add bases for all tabs

--- uitest1.py
+++ uitest1.py
@@ -15,11 +15,11 @@ class StartWindow(Tk):
         # tab buttons
         self.wordstab = Button(text="Wordcount", command=self.handle_words_press)
         self.wordstab.pack(side="left", anchor="nw")
-        self.achtab = Button(text="Achievements")
+        self.achtab = Button(text="Achievements", command=self.handle_ach_press)
         self.achtab.pack(side="left", anchor="nw")
-        self.writetab = Button(text="Writer")
+        self.writetab = Button(text="Writer", command=self.handle_write_press)
         self.writetab.pack(side="left", anchor="nw")
-        self.notetab = Button(text="Notes")
+        self.notetab = Button(text="Notes", command=self.handle_notes_press)
         self.notetab.pack(side="left", anchor="nw")
         
         #logo
@@ -31,37 +31,49 @@ class StartWindow(Tk):
         wordswindow = WordsWindow()
         self.destroy()
         wordswindow.mainloop()
+    
+    def handle_ach_press(self):
+        achwindow = AchWindow()
+        self.destroy()
+        achwindow.mainloop()
+
+    def handle_write_press(self):
+        writewindow = WriteWindow()
+        self.destroy()
+        writewindow.mainloop()
+
+    def handle_notes_press(self):
+        noteswindow = NotesWindow()
+        self.destroy()
+        noteswindow.mainloop()
 
 class WordsWindow(Tk):
     def __init__(self):
         super().__init__()
 
-        self.title("ThymeWriter")
+        self.title("ThymeWriter Wordcount")
         self.geometry("1200x700")
 
-        # search bar
-        self.searchbar = Entry()
-        self.searchbar.pack(side="right", anchor="ne")
+class AchWindow(Tk):
+    def __init__(self):
+        super().__init__()
 
-        # tab buttons
-        self.wordstab = Button(text="Home", command=self.handle_home_press)
-        self.wordstab.pack(side="left", anchor="nw")
-        self.achtab = Button(text="Achievements")
-        self.achtab.pack(side="left", anchor="nw")
-        self.writetab = Button(text="Writer")
-        self.writetab.pack(side="left", anchor="nw")
-        self.notetab = Button(text="Notes")
-        self.notetab.pack(side="left", anchor="nw")
-        
-        #logo
-        self.logoimage = PhotoImage(file='./assets/thymewriterlogo3.png')
-        self.logolabel = Label(image=self.logoimage)
-        self.logolabel.pack()
+        self.title("ThymeWriter Achievements")
+        self.geometry("1200x700")
 
-    def handle_home_press(self):
-        window = StartWindow()
-        self.destroy()
-        window.mainloop()
+class WriteWindow(Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("ThymeWriter Text Editor")
+        self.geometry("1200x700")
+
+class NotesWindow(Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("ThymeWriter Worldbuilding Notes")
+        self.geometry("1200x700")
 
 # Start the event loop.
 window = StartWindow()
